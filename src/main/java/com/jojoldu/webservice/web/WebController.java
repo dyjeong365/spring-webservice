9db168c2cd6b01ac1ee8e49d7570ef8a7d8b9441
dyjeong365 <dyjeong365@gmail.com>
2023-06-13 13:29:26 +0900

    refactor: remove unused annotation

--- src/main/java/com/jojoldu/webservice/web/WebController.java
+++ src/main/java/com/jojoldu/webservice/web/WebController.java
@@ -1,14 +1,12 @@
 package com.jojoldu.webservice.web;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class WebController {
 
-    @GetMapping("/")
+    @GetMapping
     public String main() {
         return "main";
     }
